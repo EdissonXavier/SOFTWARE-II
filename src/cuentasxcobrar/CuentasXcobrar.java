f6f87f1f2aaf2d64071fa1c412045de0db8c9780
EdissonXavier <35033700+EdissonXavier@users.noreply.github.com>
2018-01-08 09:39:31 -0500

    Update CuentasXcobrar.java

--- src/cuentasxcobrar/CuentasXcobrar.java
+++ src/cuentasxcobrar/CuentasXcobrar.java
@@ -19,6 +19,8 @@ public class CuentasXcobrar {
         System.out.println("HOLA MUNDOOO");
         
         System.out.println("SOY EDISSON");
+        
+        System.out.println("Codigo enviado desde GitHub a Java");
     }
     
 }
